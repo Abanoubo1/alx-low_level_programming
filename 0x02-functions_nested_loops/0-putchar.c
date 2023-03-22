fef602070651cf6f5cf45c9a7cd3e024e467b426
Abanoubo1 <abanoubwageh88@gmail.com>
2023-03-21 17:43:31 -0700

    all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- * main - Program that prints _putchar
- * followed by a new line
- * Return: 0 (Success)
- */
+* main - Entry point
+*
+* Description: print value of n status; greater than 5,
+* is zero or less than 6 and not equal 0
+*
+* Return: 0 (End Program)
+*/
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int start = 0;
+	char str[] = "_putchar";
+
+	while (start < 8)
+	{
+		_putchar(str[start]);
+		start++;
+	}
 	_putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include"main.h"
 
 /**
@@ -20,4 +21,4 @@ void print_to_98(int n)
 		for (count = n; count < 98; ++count)
 			printf("%d, ", count);
 	printf("98\n");
-}}
+}
