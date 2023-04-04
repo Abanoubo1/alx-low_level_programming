738c779908a7efa9f9c58c31905031c1535549a9
Abanoubo1 <abanoubwageh88@gmail.com>
2023-04-03 17:09:44 -0700

    fix again too

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,12 +12,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			_puthar(a[i][j]);
-		_putchar('\n')
+			_putchar(a[i][j]);
+		_putchar('\n');
 	}
-
-
-
-
-
 }
