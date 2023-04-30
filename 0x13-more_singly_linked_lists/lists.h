bf2fa5b512573cdcb705b249497627476f97a994
Abanoubo1 <abanoubwageh88@gmail.com>
2023-04-30 13:33:37 -0700

    fix in l comm

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,8 +10,15 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * 
  */
+
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 typedef struct listint_s
 {
 	int n;
