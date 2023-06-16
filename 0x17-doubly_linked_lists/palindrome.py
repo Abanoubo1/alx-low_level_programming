7a2328fd9e7bec28a12985e45544a0a3972467e6
Abanoubo1 <abanoubwageh88@gmail.com>
2023-06-15 18:31:38 -0700

    10. Palindromes123

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-def ispalindrome(n):
-    string = str(n)
-    return string == string[::-1]
+largest_palindrome = 0
 
-array = []
-for i in range(999, 99, -1):
-    for j in range(i, 99, -1):
-        if ispalindrome(i * j):
-            array.append(i * j)
-print(max(array))
+# Loop through all possible pairs of 3-digit numbers
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        # Calculate the product of the two numbers
+        product = i * j
+        # Convert the product to a string and check if it's a palindrome
+        if str(product) == str(product)[::-1]:
+            # If it's a palindrome, check if it's larger than the current largest palindrome
+            if product > largest_palindrome:
+                largest_palindrome = product
+
+# Save the result to a file
+with open('102-result', 'w') as f:
+    f.write(str(largest_palindrome))
+
